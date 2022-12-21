e9bd1db1bc13ee67900969867b693a83912f70bf
santoshkumarradha <santosh@agnostiq.ai>
2022-12-21 03:00:57 -0500

    version update

--- pysktb/__init__.py
+++ pysktb/__init__.py
@@ -4,7 +4,7 @@ from .lattice import Lattice
 from .system import System
 from .hamiltonian import Hamiltonian
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 
 __all__ = ["Structure", "Atom", "Lattice", "System", "Hamiltonian"]
